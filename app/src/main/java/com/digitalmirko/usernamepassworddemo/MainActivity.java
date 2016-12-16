fcce52376c34c420bb74a7a98700411c04a15315
Mirko Cukich <mirkocukich@gmail.com>
2016-12-16 14:44:22 -0600

    Btn clicked test

--- app/src/main/java/com/digitalmirko/usernamepassworddemo/MainActivity.java
+++ app/src/main/java/com/digitalmirko/usernamepassworddemo/MainActivity.java
@@ -2,8 +2,16 @@ package com.digitalmirko.usernamepassworddemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void btnClicked(View view) {
+
+
+        Log.i("Info","Btn clicked Testing123...");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
