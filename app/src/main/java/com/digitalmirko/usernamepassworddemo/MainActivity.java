22f51ec6a9f30a046ba25d86ea75834ce507aa31
Mirko Cukich <mirkocukich@gmail.com>
2016-12-16 14:52:24 -0600

    Program Complete with comments

--- app/src/main/java/com/digitalmirko/usernamepassworddemo/MainActivity.java
+++ app/src/main/java/com/digitalmirko/usernamepassworddemo/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Button action method
     public void btnClicked(View view) {
 
         // Username TextField
